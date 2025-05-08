69f34840849b0a585d16c7f079289d9fe0b54b2d
Josep-Oriol <joseporiol.ferrufino@inslapineda.cat>
2025-05-08 08:52:53 +0200

    backend rutas -> verUser por ID, updateUser por ID y delete User

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -10,6 +10,50 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
+export const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+    res.json(user);
+  } catch (err) {
+    console.log(`Error al obtener el usuario, error: ${err}`);
+    res.status(500).json({ error: "Error al obtener el usuario" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedUser) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+    res.json(updatedUser);
+  } catch (err) {
+    console.log(`Error al actualizar el usuario, error: ${err}`);
+    res.status(500).json({ error: "Error al actualizar el usuario" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await User.findByIdAndDelete(id);
+    if (!deletedUser) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+    res.json({ message: "Usuario eliminado correctamente" });
+  } catch (err) {
+    console.log(`Error al eliminar el usuario, error: ${err}`);
+    res.status(500).json({ error: "Error al eliminar el usuario" });
+  }
+};
+
 /*export const createUser = async (req, res) => {
   try {
     console.log(req.body);
